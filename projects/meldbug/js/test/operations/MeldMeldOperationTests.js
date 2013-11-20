e2ef5e07172a267f7e5ebd86cb2faae618b48f48
Sung Choi <m.sung.choi@gmail.com>
2013-11-20 00:00:52 -0800

    Adds test for MeldMeldOperation#apply.

--- projects/meldbug/js/test/operations/MeldMeldOperationTests.js
+++ projects/meldbug/js/test/operations/MeldMeldOperationTests.js
@@ -6,6 +6,8 @@
 
 //@Require('bugmeta.BugMeta')
 //@Require('bugunit-annotate.TestAnnotation')
+//@Require('meldbug.MeldBucket')
+//@Require('meldbug.MeldDocument')
 //@Require('meldbug.MeldKey')
 //@Require('meldbug.MeldMeldOperation')
 
@@ -23,6 +25,8 @@ var bugpack                 = require('bugpack').context();
 
 var BugMeta                 = bugpack.require('bugmeta.BugMeta');
 var TestAnnotation          = bugpack.require('bugunit-annotate.TestAnnotation');
+var MeldBucket              = bugpack.require('meldbug.MeldBucket');
+var MeldDocument            = bugpack.require('meldbug.MeldDocument');
 var MeldKey                 = bugpack.require('meldbug.MeldKey');
 var MeldMeldOperation       = bugpack.require('meldbug.MeldMeldOperation');
 
@@ -46,11 +50,8 @@ var meldMeldOperationInstantiationTest = {
     //-------------------------------------------------------------------------------
 
     setup: function(test) {
-        this.id             = "testId";
-        this.meldType       = "TestType";
-        this.filter         = "basic";
-        this.testMeld       = {};
-        this.meldKey        = new MeldKey(this.meldType, this.id, this.filter);
+        this.testMeld           = {};
+        this.meldKey            = new MeldKey("TestType", "testId", "basic");
         this.meldMeldOperation  = new MeldMeldOperation(this.meldKey, this.testMeld);
     },
 
@@ -70,4 +71,48 @@ var meldMeldOperationInstantiationTest = {
 };
 bugmeta.annotate(meldMeldOperationInstantiationTest).with(
     test().name("MeldMeldOperation - instantiation Test")
+);
+
+var meldMeldOperationApplyTest = {
+
+    //-------------------------------------------------------------------------------
+    // Setup Test
+    //-------------------------------------------------------------------------------
+
+    setup: function(test) {
+        this.meldKey            = new MeldKey("TestType", "testId", "basic");
+        this.meldBucket         = new MeldBucket();
+        this.testMeld           = new MeldDocument(this.meldKey, {});
+        this.testMeldTwo        = new MeldDocument(this.meldKey, {});
+        this.meldMeldOperation  = new MeldMeldOperation(this.meldKey, this.testMeld);
+        this.duplicateOperation = new MeldMeldOperation(this.meldKey, this.testMeldTwo);
+    },
+
+    //-------------------------------------------------------------------------------
+    // Run Test
+    //-------------------------------------------------------------------------------
+
+    test: function(test) {
+        test.assertFalse(this.testMeld.getMeldBucket(),
+            "Assert meld does not initially have its meldBucket property set");
+        var meld = this.meldMeldOperation.apply(this.meldBucket);
+        test.assertTrue(meld.getMeldBucket(),
+            "Assert the meld has the meldBucket set on them after apply");
+
+        test.assertFalse(this.testMeldTwo.getMeldBucket(),
+            "Assert meld does not initially have its meldBucket property set");
+        var meldTwo = this.duplicateOperation.apply(this.meldBucket);
+        test.assertTrue(meldTwo.getMeldBucket(),
+            "Assert duplicate MeldMeldOperations have the meldBucket set on them after apply");
+
+        test.assertTrue(this.meldBucket.containsMeld(this.testMeld),
+            "Assert the meldBucket contains the meld by meldKey");
+        test.assertEqual(this.meldBucket.getMeld(this.meldKey), this.testMeld,  //failing getMeld is returning undefined
+            "Assert the meldBucket contains the original MeldDocument");
+        test.assertNotEqual(this.meldBucket.getMeld(this.meldKey), this.testMeldTwo,
+            "Assert the meldBucket does not contain the duplicate MeldDocument");
+    }
+};
+bugmeta.annotate(meldMeldOperationApplyTest).with(
+    test().name("MeldMeldOperation - #apply Test")
 );
